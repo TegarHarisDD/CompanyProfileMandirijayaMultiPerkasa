c007eddfd9b5d753b9dfa2d36da17cd2e2b598f7
Tegar Haris DD <tegarharisdd@gmail.com>
2026-04-03 22:40:51 +0700

    Switch-to-HashRouter-for-github-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation, Navigate, Outlet } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation, Navigate, Outlet } from 'react-router-dom';
 
 // --- LAYOUTS ---
 import Navbar from './components/Navbar';
